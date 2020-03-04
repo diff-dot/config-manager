8ce78d571d8adc0106576850c885e6636c4f5a56
Hyogeun Wang <w@endless9.com>
2020-03-04 17:42:48 +0900

    템플릿 분리

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,4 @@ export * from './ConfigManager';
 export * from './ConfigTemplate';
 
 // Config Templates
-export * from './templates/RedisConfig';
-export * from './templates/ElasticSearchConfig';
-export * from './templates/CORSConfig';
-export * from './templates/CryptoConfig';
-export * from './templates/S3Config';
+export * from './templates';
---
+++ src/templates/index.ts
@@ -0,0 +1,4 @@
+export * from './ElasticSearchConfig';
+export * from './CORSConfig';
+export * from './CryptoConfig';
+export * from './S3Config';
